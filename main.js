da26791c09ba6658959067915eabcb5fc57d2147
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-06-12 17:31:08 +0300

    css update

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Backend Developer", "full Stack developer","software engineer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
