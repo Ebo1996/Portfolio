e6bfbded1da6136485b79c4be5e0b0e9dec10730
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-06-14 20:54:52 +0300

    commit

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ var typed = new Typed(".text", {
   backDelay: 1000,
   loop: true,
 });
+
+
+
+function animateProject(element) {
+  document.querySelectorAll(".project-item").forEach((item) => {
+    if (item !== element) {
+      item.classList.remove("animated", "zoomed");
+    }
+  });
+
+  element.classList.toggle("animated");
+  element.classList.toggle("zoomed");
+
+  if (element.classList.contains("animated")) {
+    setTimeout(() => {
+      element.classList.remove("animated", "zoomed");
+    }, 3000);
+  }
+}
+
+document.querySelectorAll(".project-item").forEach((item) => {
+  item.addEventListener("touchstart", function () {
+    this.classList.add("hover-effect");
+  });
+
+  item.addEventListener("touchend", function () {
+    setTimeout(() => {
+      this.classList.remove("hover-effect");
+    }, 500);
+  });
+});
